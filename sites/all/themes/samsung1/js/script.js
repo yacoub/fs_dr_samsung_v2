611c0184d534f3210a459f15be55baf99b779215
Amer Shaikh Salem <amer.shaikhsalem@foresite.jo>
2012-12-23 18:38:46 +0300

    minor change

--- sites/all/themes/samsung1/js/script.js
+++ sites/all/themes/samsung1/js/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    
+    function SubInit(msgTitle, msgBody){    
+    $('.activator-btn').click(function(e){
+        e.preventDefault();
+        $('#overlay').fadeIn('fast',function(){
+            $('#box').animate({
+                'top':'160px'
+            },500);
+        });
+    });
+    };
 });
--- sites/all/themes/samsung1/page.tpl.php
+++ sites/all/themes/samsung1/page.tpl.php
@@ -10,27 +10,49 @@
                         <div class="content-card-wrapper">
                             <div class="content-card">
                                 <?php
-                                if(isset($node))
-                                if ($node->type == "webform"): ?>
-                                    <div class="form_bg content-card-padding">
+                                if (isset($node))
+                                    if ($node->type == "webform"):
+                                        ?>
+                                        <div class="form_bg content-card-padding">
                                         <?php print render($page['content']); ?>
-                                    </div>
-                                <?php else: ?>
-                                    <div class="content-card-padding">
+                                        </div>
+                                        <?php else: ?>
+                                        <div class="content-card-padding">
                                         <?php print render($page['content']); ?>
-                                    </div>
-                                <?php endif; ?>
+                                        </div>
+                            <?php endif; ?>
                             </div>
                             <?php if (isset($node->field_next_link["und"])): ?>
-                            <a href="<?php print $node->field_next_link["und"][0]["value"] ?>" id="next_btn" class="sprite1"></a>
+                                <a href="<?php print $node->field_next_link["und"][0]["value"] ?>" id="next_btn" class="sprite1"></a>
                             <?php endif; ?>
                             <?php if (isset($node->field_prev_link["und"])): ?>
                                 <a href="<?php print $node->field_prev_link["und"][0]["value"] ?>" id="prev_btn" class="sprite1"></a>
-                            <?php endif; ?>
+<?php endif; ?>
                         </div>
+
+                        <?php if(isset($messages)): ?>
+                        <div class="overlay" id="overlay" style="display:none;"></div>
+                        <div class="box" id="msg-box">
+                            <a class="boxclose" id="msgboxclose"></a>
+                            <h1 class="msg-title">
+
+                            </h1>
+                            <div class="vSpace10"></div>
+                            <p class="msg-body">
+                                  <?php print $messages; ?>
+                            </p>
+                            <div class="vSpace10"></div>
+                            <p class="msg-action">
+                                <a href="#close" class="inline-block close-button"><?php echo ($lang == "ar" ? "إغلاق" : "Close"); ?></a>
+                            </p>
+                        </div> 
+                        <?php endif; ?>
+
+
+
+
                         <?php if (isset($user->uid)): ?>
                             <?php if ($user->uid != 0): ?>
-                                <?php print $messages; ?>
                                 <?php print render($title_prefix); ?>
                                 <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
                                 <?php print render($title_suffix); ?>
@@ -38,7 +60,7 @@
                                 <?php print render($page['help']); ?>
                                 <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>                                    
                             <?php endif; ?>
-                        <?php endif; ?>
+<?php endif; ?>
                     </div></div> <!-- /.section, /#content -->
 
             </div></div> <!-- /#main, /#main-wrapper -->
@@ -46,3 +68,5 @@
         <div id="footer"><div class="section">
             </div></div> <!-- /.section, /#footer -->
     </div></div> <!-- /#page, /#page-wrapper -->
+<script type="text/javascript" src="<?php print base_path() . path_to_theme(); ?>/js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="<?php print base_path() . path_to_theme(); ?>/js/script.js"></script>
