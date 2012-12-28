fecc18f92fb222e29801afa0a5320d90df10cea1
Yacoub Oweis <yacoub.oweis@gmail.com>
2012-12-28 23:04:23 +0300

    Applied new changes requested by the client 4

--- sites/all/themes/samsung1/tpl_system.php
+++ sites/all/themes/samsung1/tpl_system.php
@@ -1,23 +1,39 @@
-<div id="content" class="column"><div class="section">                                            
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-            <h1 class="title" id="page-title">
-                <?php print $title; ?>
-            </h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>                                            
-        <?php if ($tabs): ?>
-            <div class="tabs">
-                <?php print render($tabs); ?>
+<?php include 'detect_device.php'; $flag = "no"; ?>
+<div id="header">
+    <div class="section clearfix">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+            <img src="<?php echo base_path() . path_to_theme(); ?>/logo.png" alt="<?php print t('Home'); ?>" width="155" height="55"/>
+        </a>
+    </div>
+</div>
+<div id="page-wrapper">
+    <div id="page" class="contact-page">
+        <div id="main-wrapper">
+            <div id="main" class="clearfix">
+                <div id="content" class="column">
+                    <div class="section">
+                        <?php print render($title_prefix); ?>
+                        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+                        <?php print render($title_suffix); ?>
+                        <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+                        <a id="main-content"></a>
+                        <div class="system-page">
+                            <?php print render($page['content']); ?>
+                        </div>
+                        <?php if (isset($user->uid)): ?>
+                            <?php if ($user->uid != 0): ?>
+                                <?php print render($page['help']); ?>
+                                <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>                                    
+                            <?php endif; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
             </div>
-        <?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-            <ul class="action-links">
-                <?php print render($action_links); ?>
-            </ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-    </div></div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    var flag = "<?php print $flag ?>";
+</script>
+<script type="text/javascript" src="<?php print base_path() . path_to_theme(); ?>/js/jquery-1.8.3.min.js"></script>
+<script type="text/javascript" src="<?php print base_path() . path_to_theme(); ?>/js/script.js"></script>
